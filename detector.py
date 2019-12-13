035a4b576cd50710ea843a4788fed34544d67a5b
Amit Agrawal <amita3@illinois.edu>
2019-12-12 21:47:39 -0500

    1) Made path OS agnostic 2) checking for 'input' folder, if does not exists then creating it.

--- detector.py
+++ detector.py
@@ -1,11 +1,16 @@
 #import sys
 from newswatcher import NewsWatcher
 from database import Database
+import os
 
 def main():
-    src_path = '.\input'
-    db_path = '.\data'
+    src_path =  os.path.join('.','input')
+    db_path = os.path.join('.', 'data')
     print('started ......')
+
+    if not os.path.exists(src_path):
+        os.mkdir(src_path)
+    print('Input Path: '%os.path.abspath(src_path))
 
     db = Database(db_path)
     db.clear_alldata()
